bf8472cb1b40015e1361982958166d433fd3c445
Mike Santiago <miketheripper1@gmail.com>
2015-06-19 13:34:47 -0400

    Add a todo note on EntityMapping.java

--- src/com/mikesantiago/javatextengine/Core/EntityMapping.java
+++ src/com/mikesantiago/javatextengine/Core/EntityMapping.java
@@ -2,4 +2,5 @@ package com.mikesantiago.javatextengine.Core;
 
 public class EntityMapping
 {
+	//TODO: Write an entity map double array to contain entities spawning at their proper x/y coordinates
 }
