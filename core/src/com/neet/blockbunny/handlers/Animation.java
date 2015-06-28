d4f92f0c86c0d9be06c6252f41f4e851052f1188
Mike Santiago <miketheripper1@msn.com>
2015-06-28 19:49:58 -0400

    basic animation

---
+++ core/src/com/neet/blockbunny/handlers/Animation.java
@@ -0,0 +1,75 @@
+package com.neet.blockbunny.handlers;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * Handles animated sprites using an array of TextureRegions.
+ */
+public class Animation {
+	
+	/**
+	* Basic implementation
+	Texture tex = Game.res.getTexture("crystal");
+		TextureRegion[] sprites = TextureRegion.split(tex, 16, 16)[0];
+		animation.setFrames(sprites, 1 / 30f);
+		
+		width = sprites[0].getRegionWidth();
+		height = sprites[0].getRegionHeight();
+		
+	*/
+	
+	private TextureRegion[] frames;
+	private float time;
+	private float delay;
+	private int currentFrame;
+	
+	private int timesPlayed;
+	
+	public Animation() {}
+	
+	public Animation(TextureRegion[] frames) {
+		this(frames, 1 / 12f);
+	}
+	
+	public Animation(TextureRegion[] frames, float delay) {
+		this.frames = frames;
+		this.delay = delay;
+		time = 0;
+		currentFrame = 0;
+	}
+	
+	public void setDelay(float f) { delay = f; }
+	public void setCurrentFrame(int i) { if(i < frames.length) currentFrame = i; }
+	public void setFrames(TextureRegion[] frames) {
+		setFrames(frames, 1 / 12f);
+	}
+	public void setFrames(TextureRegion[] frames, float delay) {
+		this.frames = frames;
+		time = 0;
+		currentFrame = 0;
+		timesPlayed = 0;
+		this.delay = delay;
+	}
+	
+	public void update(float dt) {
+		if(delay <= 0) return;
+		time += dt;
+		while(time >= delay) {
+			step();
+		}
+	}
+	
+	private void step() {
+		time -= delay;
+		currentFrame++;
+		if(currentFrame == frames.length) {
+			currentFrame = 0;
+			timesPlayed++;
+		}
+	}
+	
+	public TextureRegion getFrame() { return frames[currentFrame]; }
+	public int getTimesPlayed() { return timesPlayed; }
+	public boolean hasPlayedOnce() { return timesPlayed > 0; }
+	
+}
